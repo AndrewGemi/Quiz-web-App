f16e44040d176f32969dab0aee5d443760a3a4b6
Andrew <andrewgamal7@gmail.com>
2026-08-18 18:30:56 +0300

    fixed build error

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Options from "./Options";
 
 /* Split an answer string into parts for blanks. */
